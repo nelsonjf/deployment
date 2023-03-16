349107d3935481955ef3b7d3cfaa8338c32ff737
James <jfnelson2000@gmail.com>
2023-03-16 09:40:54 -0700

    REST-Rant port

---
+++ views/places/show.jsx
@@ -0,0 +1,94 @@
+const React = require('react')
+const Def = require('../default')
+
+function Show (data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+    let rating = (
+        <h3 className='inactive'>
+            Not yet rated
+        </h3>
+    )
+    if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += '⭐'
+        }
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )
+
+        comments = data.place.comments.map(c => {
+            return (
+                <div className='border'>
+                    <h2 className='rant'>{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
+    return (
+        <Def>
+            <main>
+                <img src={data.place.pic} alt={data.place.name}/>
+                <p>Located at {data.place.city}, {data.place.state}</p>
+                <h1>{data.place.name}</h1>
+                <h3>Rating</h3>
+                    {rating}
+                <h3>Description</h3>
+                    <p>{data.place.showEstablished()}</p>
+                    <p>Serving {data.place.cuisines}</p>
+                <h3>Comments</h3>
+                    {comments}
+                <a href={`/places/${data.place.id}/edit`} className='btn btn-warning'>
+                    Edit    
+                </a>
+                <form method='POST' action={`/places/${data.place.id}?_method=DELETE`}>
+                    <button type='submit' className='btn btn-danger'>
+                        Delete
+                    </button>
+                </form>
+                <h2>Got Your Own Rant or Rave?</h2>
+                    <form action={`/places/${data.place.id}/comment`} method="POST">
+                    <div className="row">
+                        <div className="form-group col-sm-12">
+                        <label htmlFor="content">Content</label>
+                        <textarea id="content" name="content" className="form-control"></textarea>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group col-sm-4">
+                        <label htmlFor="author">Author</label>
+                        <input id="author" name="author" className="form-control" />
+                        </div>
+                        <div className="form-group col-sm-4">
+                        <label htmlFor="stars">Star Rating</label>
+                        <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                        </div>
+                        <div className="form-group col-sm-2">
+                        <label htmlFor="rant">Rant?</label>
+                        <input type="checkbox" id="rant" name="rant" className="form-control" />
+                        </div>
+                    </div>
+                    <input type="submit" className="btn btn-primary" value="Add Comment" />
+                    </form>
+            </main>
+        </Def>
+        
+    )
+}
+
+module.exports = Show
